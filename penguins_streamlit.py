7efd15db6bcd3a4c1a2f396b04178d700b04c95d
Wilson Usman <usmanwilson@gmail.com>
2021-09-04 10:09:46 -0400

    add password to app

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -7,10 +7,16 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-st.title('Penguin Classifier')
+st.title('Penguin Classifier: A Machine Learning App')
 st.write("This app uses 6 inputs to predict the species of penguin using "
          "a model built on the Palmer's Penguin's dataset. Use the form below"
          " to get started!")
+
+password_guess = st.text_input('What is the Password?')
+if password_guess != st.secrets["password"]:
+    st.stop()
+
+
 penguin_file = st.file_uploader('Upload your own penguin data')
 
 if penguin_file is None:
