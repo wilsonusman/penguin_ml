87fe9526489ba6dd6d3240ac8471c4fb6fb9c695
Wilson Usman <usmanwilson@gmail.com>
2021-09-04 08:31:21 -0400

    changed our title

--- penguins_ml.py
+++ penguins_ml.py
@@ -6,7 +6,7 @@ import pickle
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-st.title('Penguin Classifier')
+st.title('Penguin Classifier: A Machine Learning App')
 st.write("This app uses 6 inputs to predict the species of penguin using "
          "a model built on the Palmer's Penguin's dataset. Use the form below"
          " to get started!")
