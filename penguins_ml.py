dddd23c7f09801d448ca873e873be69b0124e680
Wilson Usman <usmanwilson@gmail.com>
2021-09-04 09:00:45 -0400

    add password to app

--- penguins_ml.py
+++ penguins_ml.py
@@ -10,6 +10,11 @@ st.title('Penguin Classifier: A Machine Learning App')
 st.write("This app uses 6 inputs to predict the species of penguin using "
          "a model built on the Palmer's Penguin's dataset. Use the form below"
          " to get started!")
+
+password_guess = st.text_input('What is the Password?')
+if password_guess != st.secrets["password"]:
+  st.stop()
+
 penguin_file = st.file_uploader('Upload your own penguin data')
 
 penguin_df = pd.read_csv('penguins.csv')
